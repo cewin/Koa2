8c630b79e699e4c615c6735ec4347302bb359301
szy0syz <szy0syz@gmail.com>
2018-05-16 13:42:22 +0800

    feat: 新增 HTML 静态模板文件

---
+++ server/tpl/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+  <DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Koa Server HTML</title>
+      <link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+      <script src="https://cdn.bootcss.com/jquery/3.3.0/jquery.min.js"></script>
+      <script src="https://cdn.bootcss.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Hi Jerry</h1>
+            <p>This is JJJJJJJJ</p>
+          </div>
+          <div class="col-md-4">
+            <p>测试敬爱 html 页面</p>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+`
