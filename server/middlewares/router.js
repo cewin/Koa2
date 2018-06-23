a76f3e9a2346b96ec18aefb60437ee90fbb22052
szy0syz <szy0syz@gmail.com>
2018-06-24 01:06:57 +0800

    update [9-5] 完成api接口

--- server/middlewares/router.js
+++ server/middlewares/router.js
@@ -1,7 +1,12 @@
-import { resolve } from "path"
+const { resolve } = require("path")
 const { Route } = require('../libs/decorator')
+
 // 只要执行此中间件，就等于执行了整个路由中间层
-export const router = app => {
-  const apiPath = resolve(__dirname, './routers')
-  const router = new Route(app, apiPath)
+module.exports = {
+  router: app => {
+    const apiPath = resolve(__dirname, './routers')
+    const router = new Route(app, apiPath)
+
+    router.init()
+  }
 }
