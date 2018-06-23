859a974b4be61b04126f09de9a67f937772ee1f9
szy0syz <szy0syz@gmail.com>
2018-06-23 22:27:24 +0800

    feat: 引入koa-router管理路由

--- server/index.js
+++ server/index.js
@@ -2,22 +2,29 @@ const Koa = require("koa")
 const pug = require("pug")
 const views = require("koa-views")
 const { resolve } = require("path")
+
 const { connect, initSchemas } = require("./database/init")
+const Router = require('./routes')
 
 const mongoose = require('mongoose')
 
 ~(async () => {
   await connect()
   // 加载所有schemas
-  initSchemas()
+  await initSchemas()
 
-  // const Movie = mongoose.model('Movie')
+  const Movie = mongoose.model('Movie')
   // const rr = await Movie.find({})
   // require('./task/movie')
-  require('./task/api')
+  // require('./task/api')
 })()
 
-const app = new Koa();
+const app = new Koa()
+
+app
+  .use(Router.routes())
+  .use(Router.allowedMethods())
+
 app.use(
   views(resolve(__dirname, "./views"), {
     extension: "pug"
